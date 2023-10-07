fcb8ab6977fe24763ff4332e27ae1d9e25c1fc9f
SAPIPA <lbvf20035@gmail.com>
2023-10-07 21:40:29 +0300

    init commit

---
+++ src/main/java/com/prodamgarage/mindasengine/MindasengineApplication.java
@@ -0,0 +1,13 @@
+package com.prodamgarage.mindasengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindasengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindasengineApplication.class, args);
+	}
+
+}
