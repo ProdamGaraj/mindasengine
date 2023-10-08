8eda7ddc0b413e7c2b905e3af82844d99e5bd607
ProdamGaraj <prodgaraj@yandex.ru>
2023-10-08 18:25:43 +0300

    Fix commit

---
+++ back/src/main/java/com/prodamgarage/mindasengine/models/User.java
@@ -0,0 +1,22 @@
+package com.prodamgarage.mindasengine.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table( name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username")})
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+}
---
+++ back/src/test/java/com/prodamgarage/mindasengine/MindasengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodamgarage.mindasengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindasengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
