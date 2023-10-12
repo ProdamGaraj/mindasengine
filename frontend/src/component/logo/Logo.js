8e789000b6dbc46119cff990c00ecf8404f7d113
dmitriy oginov <dmitrij.oginov@mail.ru>
2023-10-13 02:21:38 +0300

    changeLogo

--- frontend/src/component/logo/Logo.js
+++ frontend/src/component/logo/Logo.js
@@ -1,10 +1,12 @@
-import LogoImg from "../../img/logo.png";
+import LogoImg from "../../img/logo2.png";
+import LogoText from "../../img/logo2text.png";
 
 export const Logo = () => {
     return(
         <>
-            <a href="/">
-                <img src={LogoImg} alt="" />
+            <a href="/" className="flex align-cent" style={{gap:'10px'}}>
+                <img src={LogoImg} alt="" style={{maxHeight:'44px', maxWidth:'44px'}}/>
+                <img src={LogoText} alt="" style={{maxHeight:'44px', maxWidth:'215px'}}/>
             </a>
         </>
     )
