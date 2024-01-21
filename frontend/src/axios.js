9c15a3644f56aba9fd775e770501020bb2fb4e5d
ProdamGaraj <prodgaraj@yandex.ru>
2024-01-21 16:00:45 +0300

    fix

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const instance = axios.create({
-baseURL: "http://mindasengine.uz",
+baseURL: "http://mindasengine.uz:1337",
 });
-const baseURL = "http://mindasengine.uz";
+const baseURL = "http://mindasengine.uz:1337";
 instance.interceptors.request.use((config) => {
     config.headers.Authorization = window.localStorage.getItem('token');
 	return config;
